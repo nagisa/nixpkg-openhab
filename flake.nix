0cedd214463a5a686eb274d59eed8e808da9042d
Simonas Kazlauskas <git@kazlauskas.me>
2023-02-22 17:09:01 +0200

    Expose an overlay output for systems to easily import
    (This, I suspect will work better with cross-compilation and is just
    plain more straightforward to work with anyhow)

--- flake.nix
+++ flake.nix
@@ -4,14 +4,21 @@
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { flake-utils, nixpkgs, ... }: flake-utils.lib.eachDefaultSystem (system: {
+    outputs = { flake-utils, nixpkgs, ... }:
+    let
+        overlay = final: prev: rec {
+            jdk-openhab = final.jdk11;
+            openhab = final.callPackage ./openhab.nix { inherit jdk-openhab; };
+            openhab-addons = final.callPackage ./openhab-addons.nix {};
+        };
+        pkgs = system: import nixpkgs { inherit system; overlays = [ overlay ]; };
+    in {
+        inherit overlay;
+    } //  flake-utils.lib.eachDefaultSystem (system: {
         packages = rec {
-            jdk-openhab = nixpkgs.legacyPackages.${system}.jdk11;
-            openhab = nixpkgs.legacyPackages.${system}.callPackage ./openhab.nix {
-                inherit jdk-openhab;
-            };
-            openhab-addons = nixpkgs.legacyPackages.${system}.callPackage ./openhab-addons.nix {};
-            default = openhab;
+            jdk-openhab = (pkgs system).jdk-openhab;
+            openhab = (pkgs system).openhab;
+            openhab-addons = (pkgs system).openhab-addons;
         };
     });
 }
